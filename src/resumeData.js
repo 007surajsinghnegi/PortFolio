02d55f45d04d088860fee15972683ff313e9a892
judgesupreme <1234surajsinghnegi@gmail.com>
2020-08-18 19:38:44 +0530

    first deploy

---
+++ src/resumeData.js
@@ -0,0 +1,95 @@
+let resumeData = {
+    "name": "Suraj Singh Negi",
+    "role": "Full Stack Developer",
+    "linkedinId":"https://www.linkedin.com/in/surajnegi420",
+    "twitterid": "https://twitter.com/drunken__monk?s=09",
+    "number": " 8433188384 ",
+    "email": " 1234surajsinghnegi@gmail.com " ,
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/surajnegi420",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/007surajsinghnegi",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"twitter",
+          "url":"https://twitter.com/drunken__monk?s=09",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am currently a final year student at Indian Institute of Information Technology,Una and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper in web deveploment with python. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"India",
+    "website":"https://github.com/007surajsinghnegi",
+    "education":[
+      {
+        "UniversityName":"Indian Institute of Information Technology,Una",
+        "specialization":"B.Tech",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2021",
+      },
+      {
+        "UniversityName":"Kendriya Vidyalaya No.6 , Jaipur",
+        "specialization":"High School",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2016",
+      }
+    ],
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Javascript"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"React Native"
+      },
+      {
+        "skillname":"NodeJs"
+      },
+    ],
+    "portfolio":[
+      {
+        "name":"Amazon-Clone",
+        "description":"Used React to create the Front End of Amazon and used Firebase for backend support (authorization and cloud database). Firebase is used to deploy the Amazon-clone.",
+        "imgurl":"https://cdn.vox-cdn.com/thumbor/kM6tkdwwl17xoMMxrxaTgk4XaY8=/0x0:2040x1360/1200x800/filters:focal(857x517:1183x843)/cdn.vox-cdn.com/uploads/chorus_image/image/63968152/acastro_181114_1777_amazon_hq2_0006.0.jpg",
+        "gitlink":"https://github.com/007surajsinghnegi/Amazon-Clone.git",
+        "livelink":"https://clone-cf51f.web.app/"
+      },
+      {
+        "name":"Instagram-Clone",
+        "description":"Used React to create the Front End of Instagram and used Firebase for backend support (authorization and cloud database). Firebase is used to deploy the Instagram-clone.",
+        "imgurl":"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSwnEYJW-xEz-zU-C81qxgdTHcTToXctChfjw&usqp=CAU",
+        "gitlink":"https://github.com/007surajsinghnegi/instagram-clone.git",
+        "livelink":"https://instagram-clone-78223.web.app/"
+      },
+      {
+        "name":"Weather App",
+        "description":"Used React to create the Front End of Weather and used OpenWeatherAPI to access the weather data of different cities. GitHub.io is used to deploy the WeatherApp.",  
+        "imgurl":"https://wi-images.condecdn.net/image/doEYpG6Xd87/crop/2040/f/weather.jpg",
+        "gitlink":"https://github.com/007surajsinghnegi/WeatherApp.git",
+        "livelink":"https://007surajsinghnegi.github.io/WeatherApp"
+      },
+      {
+        "name":"Tinder-Clone",
+        "description":"Used React to create the Front End of Tinder and used Firebase for backend support ( cloud database). Firebase is used to deploy the Tinder-clone.",  
+        "imgurl":"https://lh3.googleusercontent.com/tPSknKHPUTRNsWt-YBQpcryIUlKiu3tjCdYgO7rFnpiRtl_oosb-oIZjM8LlHCLS5qY=w412-h732-rw",
+        "gitlink":"https://github.com/007surajsinghnegi/Tinder-Clone.git",
+        "livelink":"https://tinder-clone-c1c77.web.app/"
+      },
+    ],
+  }
+  
+  export default resumeData
